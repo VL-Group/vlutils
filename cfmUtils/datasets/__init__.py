c16d13707a772cc003100755cc69bc9f58582b2e
Xiaosu Zhu <xiaosu.zhu@outlook.com>
2020-12-30 21:22:48 +0800

    add datasets

---
+++ cfmUtils/datasets/__init__.py
@@ -0,0 +1,7 @@
+"""Package of datasets.
+
+Exports:
+    Zip: Wrapper dataset behaves like `zip()`.
+    Enumerate: Wrapper dataset behaves like `enumerate()` with element-wise count.
+"""
+from .wrappers import Zip, Enumerate
---
+++ cfmUtils/datasets/wrappers.py
@@ -0,0 +1,40 @@
+"""Module of wrapper datasets."""
+from torch.utils.data import Dataset
+
+__all__ = [
+    "Zip",
+    "Enumerate"
+]
+
+class Zip(Dataset):
+    """Wrapper dataset behaves like `zip()`.
+
+    Args:
+        *data (List of Dataset): A series of datasets.
+    """
+    def __init__(self, *datas: Dataset):
+        assert len(datas) > 0
+        self.datas = datas
+
+    def __len__(self):
+        return len(self.datas[0])
+
+    def __getitem__(self, idx):
+        return tuple(d[idx] for d in self.datas)
+
+
+class Enumerate(Dataset):
+    """Wrapper dataset behaves like `enumerate()` with element-wise count.
+
+    Args:
+        data (Dataset): Any datasets.
+    """
+    def __init__(self, data: Dataset):
+        assert len(data) > 0
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return idx, self.data[idx]
