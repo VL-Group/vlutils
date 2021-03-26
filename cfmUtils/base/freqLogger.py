cb7c3a83832c1ec8d1832062b851a2c7c33a0b1e
Xiaosu Zhu <xiaosu.zhu@outlook.com>
2021-03-26 10:53:04 +0800

    add frequency logger

---
+++ cfmUtils/base/freqLogger.py
@@ -0,0 +1,43 @@
+from collections.abc import MutableMapping
+import math
+
+
+class TransformedDict(MutableMapping):
+    """A dictionary that applies an arbitrary key-altering
+       function before accessing the keys"""
+
+    def __init__(self, freq, *args, **kwargs):
+        self.store = dict()
+        self._freq = freq
+        self.update(dict(*args, **kwargs))  # use the free update to set keys
+
+    def __getitem__(self, key):
+        return self.store[self._keytransform(key)]
+
+    def __setitem__(self, key, value):
+        self.store[self._keytransform(key)] = value
+
+    def __delitem__(self, key):
+        del self.store[self._keytransform(key)]
+
+    def __iter__(self):
+        return iter(self.store)
+
+    def __len__(self):
+        return len(self.store)
+
+    def _keytransform(self, key):
+        result = math.log(key) / math.log(self._freq)
+        if result.is_integer():
+            result = int(result)
+        return result
+
+
+class FrequecyHook():
+    def __init__(self, base: int, freqAndHooks: dict):
+        self._hooks = TransformedDict(base, **freqAndHooks)
+
+    def __call__(self, step, *args, **kwArgs):
+        hook = self._hooks.get(step, None)
+        if hook is not None:
+            return hook(*args, **kwArgs)
