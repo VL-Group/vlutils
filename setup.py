7fed92be1d716b023d2a87d0a9ac834018e659a0
Xiaosu <xiaosu.zhu@outlook.com>
2022-03-10 23:03:31 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
-
+import os
 from setuptools import setup
+from distutils.util import strtobool
 
-setup()
+
+INSTALL_REQUIRES = [
+    "torch>=1.9",
+    "tqdm",
+    "rich",
+    "nvidia-ml-py3"
+] if not strtobool(os.getenv("CONDA_BUILD", "false")) else []
+
+setup(install_requires=INSTALL_REQUIRES)
