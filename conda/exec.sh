b5debc237002f923c3c0d1c112e758f9715cf99a
Xiaosu <xiaosu.zhu@outlook.com>
2022-03-11 20:30:05 +0800

    update conda-build

--- conda/exec.sh
+++ conda/exec.sh
@@ -4,6 +4,6 @@ set -ex
 set -o pipefail
 
 
-conda build -c conda-forge -c bioconda -c xiaosu-zhu --output-folder . .
+conda build -c conda-forge -c bioconda -c pytorch -c xiaosu-zhu --output-folder . .
 
 anaconda upload --label main noarch/*.tar.bz2
